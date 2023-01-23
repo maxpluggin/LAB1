3ccdab4719df1cc1bfa029fecc3b435edf9db514
PLUGGIN <Soulgr33n@gmail.com>
2023-01-23 05:50:18 +0200

    zxc

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ class Lab1 {
     public static void main(String[] args) {
 
         System.out.println("HELLO");
-        System.out.println("WORLD");
+        System.out.println("WORLD 2");
         System.out.println("Mockingbird xdxdxd");
 
     }
