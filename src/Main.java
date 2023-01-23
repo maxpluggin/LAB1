a53a9e84f0c069bbebf6bbe095dd870a3afc761e
PLUGGIN <Soulgr33n@gmail.com>
2023-01-23 05:39:36 +0200

    zxc

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
-public class Main {
+class Lab1 {
+
     public static void main(String[] args) {
-        System.out.println("zxcxdxddxdx");
+
+        System.out.println("String 1 ");
+        System.out.println("String 2 ");
+        System.out.println("String 3 ");
+
     }
 }
